1653bceef9fe89e43b22b4d47325146aadcf1d4f
Paul Gueller <paul.gueller@gmail.com>
2020-04-06 21:10:39 -0500

    cleanup spacing for readability

--- src/utilities/filterDataObject.js
+++ src/utilities/filterDataObject.js
@@ -3,12 +3,12 @@ import * as _ from "lodash";
 /* TODO: update function notation and documentation */
 const FilterDataObject = (data, filter) => {
     const tmp = [];
-        data.map(item => {
-        if(filter === null || _.includes(item.name.toString().toLowerCase(), filter.toString().toLowerCase())) {
-            tmp.push(item);
-        }
-        return null;
-        })
+    data.map(item => {
+      if( filter === null || _.includes( item.name.toString().toLowerCase(), filter.toString().toLowerCase() ) ) {
+        tmp.push(item);
+      }
+      return null;
+    })
     return tmp;
 };
 
