d97eaa372c39415588e50a679f651e716cdc4d36
Paul Gueller <paul.gueller@gmail.com>
2020-04-09 11:18:59 -0500

    refactor function to be more readable

--- src/utilities/filterDataObject.js
+++ src/utilities/filterDataObject.js
@@ -1,22 +1,24 @@
 import * as _ from "lodash"
 
 /* Summary:
-    Uses lodash in iterating over the data object
+    Expects index and filter parameters, but will return the unchanged data object if either are missing
+    Uses lodash in comparing the strings while iterating over the data object
     lodash comes along for free with React packages
     Eliminates indexed values based on the provided filter criteria
     */
 
 /* TODO: update function notation and documentation depending on team styles/standards if applicable */
-const filterDataObject = (data, index, filter) => {
-    const tmp = []
-    data.map(item => {
-      /* TODO: investigate how to import only the necessary 'includes' function instead of the whole lodash package */
-      if( filter === null || filter === undefined || _.includes( item[index].toString().toLowerCase(), filter.toString().toLowerCase() ) ) {
-        tmp.push(item)
-      }
-      return null
-    })
-    return tmp
+const filterDataObject = (data, index = '', filter = '') => {
+  /* if either string index or filter are absent, return the data unchanged */
+  if ( (index || filter) === (null || '') ) {
+    return data
+  }
+
+  /* the `includes` function expects strings, so do some work on the values so they're always strings in lower case */
+  const normalize = val => val.toString().toLowerCase()
+
+  /* TODO: investigate how to import only the necessary 'includes' function instead of the whole lodash package */
+  return data.filter( item => _.includes( normalize( item[index] ), normalize( filter ) ) )
 }
 
 export default filterDataObject
